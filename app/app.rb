c0dd43e9e65dc29592bb69c42d70f5f680f1e3c4
Richard Harrah <richard.harrah2@anthem.com>
2016-02-16 18:49:53 -0500

    adding roots

--- app/app.rb
+++ app/app.rb
@@ -8,6 +8,12 @@ module Defnotablog
 
     enable :sessions
 
+    get '/' do
+      'Hello, world!'
+    end
 
+    get :about, :map => '/about_us' do
+      render :slim, 'p This is a sample blog created to demo Padrino!'
+    end
   end
 end
